255c79ee36d904051f218a6b7406d28546e7c44e
pc-magas <katanophoros@gmail.com>
2020-02-18 12:18:34 +0200

    Travis build without using docker-compose

---
+++ travis/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOCKERFILE_ALPINE_FPM="dockerfiles/fpm_alpine/Dockerfile"
+
+SERVER_FAVOR="apache"
+
+if [ $DOCKERFILE == $DOCKERFILE_ALPINE_FPM ]; then
+    SERVER_FAVOR="fpm_alpine"
+fi
+
+DB_FLAVOR=""
+case $DB_TYPE; in
+    "mysqli") DB_FLAVOR="mysql_maria"
+    "pgsql") DB_FLAVOR="postgresql"
+    *) DB_FLAVOR="mulitbase"
+esac
+
+VERSION_TAG="-t ${DB_FLAVOR}_${SERVER_FAVOR}_${VERSION}"
+
+VERSION_TYPE_TAG=""
+
+case $VERSION; in
+    ${LATEST_LTS}) VERSION_TYPE_TAG="-t ${DB_FLAVOR}_${SERVER_FAVOR}_lts"
+    ${LATEST}) VERSION_TYPE_TAG="-t ${DB_FLAVOR}_${SERVER_FAVOR}_latest"
+esac 
+
+docker build -f ${DOCKERFILE} ${VERSION_TYPE_TAG} ${VERSION_TAG} --no-cache --force-rm
--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USER" --password-stdin
-docker-compose -f $DOCKER_COMPOSE_FILE push
+docker push 
