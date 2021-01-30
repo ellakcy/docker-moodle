59e11b51e0f360e35be67c938c235ac85bd3aa07
Dimitrios <pcmagas@disroot.org>
2021-01-30 18:25:10 +0200

    Add New lines to version in case that file does noe end with a newline.

--- travis/build.sh
+++ travis/build.sh
@@ -3,25 +3,33 @@
 
 # The script required the Following Environmental Variables:
 # DB_TYPE in order to specify the database type
-# VERSION in order to specify the moodle version
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 DOCKERFILE_ALPINE_FPM="dockerfiles/fpm_alpine/Dockerfile"
 
-while read -r VERSION; do
     echo $version
-    SERVER_FAVOR="apache"
-    if [[ $DOCKERFILE == $DOCKERFILE_ALPINE_FPM ]]; then
-        SERVER_FAVOR="fpm_alpine"
+SERVER_FAVOR="apache"
+if [[ $DOCKERFILE == $DOCKERFILE_ALPINE_FPM ]]; then
+    SERVER_FAVOR="fpm_alpine"
+fi
+
+DB_FLAVOR=""
+
+case $DB_TYPE in
+    "mysqli" ) DB_FLAVOR="mysql_maria" ;;
+    "pgsql" ) DB_FLAVOR="postgresql";;
+    *) DB_FLAVOR="mulitbase"
+esac
+
+# File must end with new line
+FILE="${SCRIPTPATH}/../versions.txt"
+echo >> "${FILE}"
+
+while read -r VERSION; do
+
+    if [ "${VERSION}" == "" ]; then
+        continue
     fi
-
-    DB_FLAVOR=""
-
-    case $DB_TYPE in
-        "mysqli" ) DB_FLAVOR="mysql_maria" ;;
-        "pgsql" ) DB_FLAVOR="postgresql";;
-        *) DB_FLAVOR="mulitbase"
-    esac
 
     VERSION_TAG="-t ellakcy/moodle:${DB_FLAVOR}_${SERVER_FAVOR}_${VERSION}"
 
@@ -42,4 +50,4 @@ while read -r VERSION; do
 
     echo "Running: docker build -f ${DOCKERFILE} ${VERSION_TYPE_TAG} ${VERSION_TAG} --no-cache --force-rm . "
     docker build --build-arg DB_TYPE=${DB_TYPE} --build-arg VERSION=${VERSION}  -f ${DOCKERFILE} ${VERSION_TYPE_TAG} ${VERSION_TAG} .
-done < "${SCRIPTPATH}/../versions.txt"
+done < "${FILE}"
