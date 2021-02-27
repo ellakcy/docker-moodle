5dcc66480386f3de93317d88994ea2aa3aea00dd
Dimitrios <pcmagas@disroot.org>
2021-02-27 21:30:25 +0200

    Set Build Trigger.

--- travis/trigger_build.sh
+++ travis/trigger_build.sh
@@ -5,7 +5,7 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 
 if [ ${TRAVIS_BRANCH} == 'dev' ] || [ ${TRAVIS_BRANCH} == 'master' ]; then
-    bash ${SCRIPTPATH}/..travis/gen_braches.sh
+    bash ${SCRIPTPATH}/../travis/gen_braches.sh
 else
     bash ${SCRIPTPATH}/../travis/buils_docker.sh
 fi
