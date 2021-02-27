1ebebdf1a0b8cc386e88ff54034a821fca07f431
Dimitrios <pcmagas@disroot.org>
2021-02-27 21:38:11 +0200

    Pushing branches

--- travis/gen_braches.sh
+++ travis/gen_braches.sh
@@ -41,7 +41,7 @@ while read VERSION; do
             git add ${SCRIPTPATH}/../DOCKERFILE
             git commit -m "SET VERSION AND FLAVOR"
             git status
-            push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${BRANCH}
+            git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${BRANCH}
             git checkout $CURR_BRANCH
         done
     done
