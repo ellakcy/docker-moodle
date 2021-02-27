f2efb90c02e5c51f9403aa647e1e43a64a601a0c
Dimitrios <pcmagas@disroot.org>
2021-02-27 21:49:43 +0200

    Pushing branches

--- travis/trigger_build.sh
+++ travis/trigger_build.sh
@@ -5,6 +5,7 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 
 if [ ${TRAVIS_BRANCH} == 'dev' ] || [ ${TRAVIS_BRANCH} == 'master' ]; then
+    echo ${GH_TOKEN}
     bash ${SCRIPTPATH}/../travis/gen_braches.sh
 else
     bash ${SCRIPTPATH}/../travis/buils_docker.sh
