5b2405f98eed1ddcd43943f80872b479e793dea9
Dimitrios <pcmagas@disroot.org>
2021-02-27 20:21:24 +0200

    Test Branch generation

--- travis/gen_braches.sh
+++ travis/gen_braches.sh
@@ -7,28 +7,40 @@ DB_FLAVORS=("all" "mysqli" "pgsql")
 DOCKERFILES=("dockerfiles/fpm_alpine/Dockerfile" "dockerfiles/apache/Dockerfile")
 
 git fetch
+CURR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo ${CURR_BRANCH}
 
 while read VERSION; do
-    for DB_FLAVOR in $DB_FLAVORS; do
-        for DOCKERFILE in $DOCKERFILES; do
+    for DB_FLAVOR in ${DB_FLAVORS[@]}; do
+        for DOCKERFILE in ${DOCKERFILES[@]}; do
             echo $VERSION
             case $DOCKERFILE in
                 "dockerfiles/fpm_alpine/Dockerfile") SERVER_FLAVOR='fpm';;
                 "dockerfiles/apache/Dockerfile") SERVER_FLAVOR='apache';;
             esac
 
-            BRANCH="version/${VERSION}_${DB_FLAVOR}_${SERVER_FLAVOR}"
+            case $CURR_BRANCH in
+                'dev') PREFIX='test';;
+                'master') PREFIX='release';;
+            esac
+
+            BRANCH="${PREFIX}/${VERSION}_${DB_FLAVOR}_${SERVER_FLAVOR}"
+            echo ${BRANCH}
+         
             if [[ $(git ls-remote origin ${BRANCH} | wc -c) -ne 0 ]]; then 
                 git checkout ${BRANCH}
             else 
                 git checkout -b ${BRANCH}
             fi
-            git merge -s ours master
-            # echo $VERSION > ${SCRIPTPATH}/../VERSION
-            # echo $DOCKERFILE > ${SCRIPTPATH}/../DOCKERFILE
-            # git commit -am "SET VERSION AND FLAVOR"
+            git merge -s ours ${CURR_BRANCH}
+            echo $VERSION > ${SCRIPTPATH}/../VERSION
+            git add ${SCRIPTPATH}/../VERSION
+            echo $DOCKERFILE > ${SCRIPTPATH}/../DOCKERFILE
+            git add ${SCRIPTPATH}/../DOCKERFILE
+            git commit -m "SET VERSION AND FLAVOR"
             git status
-            # git push origin ${BRANCH}
+            git push origin ${BRANCH}
+            git checkout $CURR_BRANCH
         done
     done
 done < "${SCRIPTPATH}/../versions.txt"
