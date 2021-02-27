b97b5be057052d16c12695c73fa218ac23d44560
Dimitrios <pcmagas@disroot.org>
2021-02-27 19:30:51 +0200

    Create Branches Per version.

---
+++ travis/gen_braches.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+DB_FLAVORS=("all","mysqli","pgsql")
+DOCKERFILES=("dockerfiles/fpm_alpine/Dockerfile","dockerfiles/apache/Dockerfile")
+
+git fetch
+
+while read $VERSION; do
+    for DB_FLAVOR in $DB_FLAVORS; do
+        for DOCKERFILE in $DOCKERFILES; do
+
+            case $DOCKERFILE in
+                "dockerfiles/fpm_alpine/Dockerfile") SERVER_FLAVOR='fpm';;
+                "dockerfiles/apache/Dockerfile") SERVER_FLAVOR='apache';;
+            esac
+
+            BRANCH="version/${VERSION}_${DB_FLAVOR}_${SERVER_FLAVOR}"
+            if [[ $(git ls-remote origin ${BRANCH} | wc -c) -ne 0 ]]; then 
+                git checkout ${BRANCH}
+            else 
+                git checkout -b ${BRANCH}
+            fi
+            git merge -s ours master
+            # echo $VERSION > ${SCRIPTPATH}/../VERSION
+            # echo $DOCKERFILE > ${SCRIPTPATH}/../DOCKERFILE
+            # git commit -am "SET VERSION AND FLAVOR"
+            git status
+            # git push origin ${BRANCH}
+        done
+    done
+done < "${SCRIPTPATH}/../versions.txt"
