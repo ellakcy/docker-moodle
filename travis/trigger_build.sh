ec905619e133fe853f139abc1226bbef977f268a
Dimitrios <pcmagas@disroot.org>
2021-02-27 21:19:00 +0200

    Test build.

--- travis/trigger_build.sh
+++ travis/trigger_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [[ ${TRAVIS_BRANCH} == 'dev' ] or [ ${TRAVIS_BRANCH} == 'master' ]]; then
+if [ ${TRAVIS_BRANCH} == 'dev' ] || [ ${TRAVIS_BRANCH} == 'master' ]; then
  echo "Ι will generate branches"
 else
  echo "Ι will build docker"
