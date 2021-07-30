fd9e8bcb35c533bc127ede0225c178cf98b3c684
Dimitrios <pcmagas@disroot.org>
2021-07-30 22:41:05 +0300

    Repush

--- .github/build_docker.sh
+++ .github/build_docker.sh
@@ -26,30 +26,34 @@ DB_FLAVOR=""
 case $DB_TYPE in
     "mysqli" ) DB_FLAVOR="mysql_maria" ;;
     "pgsql" ) DB_FLAVOR="postgresql";;
-    *) DB_FLAVOR="mulitbase"
+    *) DB_FLAVOR="mulitibase"
 esac
 
-VERSION_TAG="-t ellakcy/moodle:${DB_FLAVOR}_${SERVER_FAVOR}_${VERSION}"
-
-VERSION_TYPE_TAG=""
+TAGS=("${DB_FLAVOR}_${SERVER_FAVOR}_${VERSION}")
 
 if [[ $VERSION == $LATEST_LTS ]]; then
-    VERSION_TYPE_TAG="-t ellakcy/moodle:${DB_FLAVOR}_${SERVER_FAVOR}_lts"
+    TAGS+=("${DB_FLAVOR}_${SERVER_FAVOR}_lts")
 fi
 
 if [[ $VERSION == $LATEST ]]; then
-    VERSION_TYPE_TAG="-t ellakcy/moodle:${DB_FLAVOR}_${SERVER_FAVOR}_latest -t ellakcy/moodle:${DB_FLAVOR}_${SERVER_FAVOR}" 
+    TAGS+=("${DB_FLAVOR}_${SERVER_FAVOR}_latest")
+    TAGS+=("${DB_FLAVOR}_${SERVER_FAVOR}")
     if [[ $SERVER_FAVOR == "apache" ]] && [[ $DB_FLAVOR = "mulitbase" ]]; then
-        VERSION_TYPE_TAG=" ${VERSION_TYPE_TAG} -t  ellakcy/moodle:latest"
+        TAGS+=("latest")
     fi
 fi
 
+PARAMS=${TAGS[@]/#/"-t ellakcy/moodle:"}
+
 echo "Running:" 
-echo "docker build --build-arg DB_TYPE=${DB_TYPE} -f ${DOCKERFILE} ${VERSION_TYPE_TAG} ${VERSION_TAG} --force-rm . "
-docker build --build-arg DB_TYPE=${DB_TYPE} --build-arg VERSION=${VERSION} -f ${DOCKERFILE} ${VERSION_TYPE_TAG} ${VERSION_TAG} --force-rm .
+echo "docker build --build-arg DB_TYPE=${DB_FLAVOR} -f ${DOCKERFILE} ${PARAMS} --force-rm . "
+docker build --build-arg DB_TYPE=${DB_FLAVOR} --build-arg VERSION=${VERSION} -f ${DOCKERFILE} ${PARAMS} --force-rm .
 
 BRANCH=${GITHUB_REF##*/}
 
 if [[ $BRANCH == 'master' ]]; then
-    docker image push --all-tags ellakcy/moodle
+    for tag in "${TAGS[@]}"; do
+        docker image push ellakcy/moodle:$tag
+    done
+
 fi
