12ff7bf786f25fd8562c1d4b617bbeea6858e0ee
Dimitrios <pcmagas@disroot.org>
2022-01-22 01:58:18 +0200

    Build without cache

--- .github/build_docker.sh
+++ .github/build_docker.sh
@@ -48,7 +48,7 @@ PARAMS=${TAGS[@]/#/"-t ellakcy/moodle:"}
 
 echo "Running:" 
 echo "docker build --build-arg DB_TYPE=${DB_FLAVOR} -f ${DOCKERFILE} ${PARAMS} --force-rm . "
-docker build --build-arg DB_TYPE=${DB_FLAVOR} --build-arg VERSION=${VERSION} -f ${DOCKERFILE} ${PARAMS} --force-rm .
+docker build --build-arg DB_TYPE=${DB_FLAVOR} --build-arg VERSION=${VERSION} -f ${DOCKERFILE} ${PARAMS} --force-rm --no-cache .
 
 BRANCH=${GITHUB_REF##*/}
 
