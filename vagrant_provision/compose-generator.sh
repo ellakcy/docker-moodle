5f2e351beb1f97c7726af5110bc353e2b9a526d5
pcmagas <pcmagas@disroot.org>
2023-12-23 18:38:31 +0200

    Provision VAgrant Image

---
+++ vagrant_provision/compose-generator.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get install ca-certificates software-properties-common curl
+curl -fsSL https://server.chillibits.com/files/repo/gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://repo.chillibits.com/$(lsb_release -is | awk '{print tolower($0)}')-$(lsb_release -cs) $(lsb_release -cs) main"
+sudo apt-get update
+sudo apt-get install compose-generator
