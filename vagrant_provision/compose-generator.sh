87bb75f38e56a591f231ebd33b79ea7a586b5dce
pcmagas <pcmagas@disroot.org>
2023-12-23 19:24:00 +0200

    Docker Provisioner

--- vagrant_provision/compose-generator.sh
+++ vagrant_provision/compose-generator.sh
@@ -6,4 +6,4 @@ sudo apt-get install ca-certificates software-properties-common curl
 curl -fsSL https://server.chillibits.com/files/repo/gpg | sudo apt-key add -
 sudo add-apt-repository "deb https://repo.chillibits.com/$(lsb_release -is | awk '{print tolower($0)}')-$(lsb_release -cs) $(lsb_release -cs) main"
 sudo apt-get update
-sudo apt-get install compose-generator
+sudo apt-get install -y compose-generator
