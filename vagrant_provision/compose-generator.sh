29efeae7498201c9cea702680b24ffb558d3c648
pcmagas <pcmagas@disroot.org>
2023-12-23 20:07:27 +0200

    Attempt to install composer generator.

--- vagrant_provision/compose-generator.sh
+++ vagrant_provision/compose-generator.sh
@@ -2,8 +2,12 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get install ca-certificates software-properties-common curl
+apt-get install ca-certificates software-properties-common curl
 curl -fsSL https://server.chillibits.com/files/repo/gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://repo.chillibits.com/$(lsb_release -is | awk '{print tolower($0)}')-$(lsb_release -cs) $(lsb_release -cs) main"
-sudo apt-get update
-sudo apt-get install -y compose-generator
+add-apt-repository "deb https://repo.chillibits.com/$(lsb_release -is | awk '{print tolower($0)}')-$(lsb_release -cs) $(lsb_release -cs) main"
+
+# Legacy key is added above we convert into new apty keyring type
+for KEY in $(apt-key --keyring /etc/apt/trusted.gpg list | grep -E "(([ ]{1,2}(([0-9A-F]{4}))){10})" | tr -d " " | grep -E "([0-9A-F]){8}\b" ); do K=${KEY:(-8)}; apt-key export $K | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/imported-from-trusted-gpg-$K.gpg; done
+
+apt-get update
+apt-get install -y compose-generator
