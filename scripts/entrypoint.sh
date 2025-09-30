373d1d8a29bc1ffb5db0f13f886f4028cc281bb4
pcmagas <pcmagas@disroot.org>
2025-09-30 22:36:51 +0300

    Setup DB

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -180,6 +180,7 @@ echo "Installing moodle"
 php /var/www/html/admin/cli/install_database.php \
           --adminemail=${MOODLE_ADMIN_EMAIL} \
           --adminuser=${MOODLE_ADMIN} \
+          --adminpass=${MOODLE_ADMIN_PASSWORD} \
           --agree-license
 
 php admin/cli/purge_caches.php
